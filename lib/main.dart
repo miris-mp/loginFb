4db5598a50a0f5a69a2cdb1c13e136e4b08f4593
teoful <tful@hotmail.it>
2020-08-10 22:04:26 +0200

    prima prova di gestione login precedente e logout --> errore login failed (catch di riga 38)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'dart:async';
 
 GoogleSignIn _googleSignIn = GoogleSignIn(
   scopes: <String>[
@@ -10,26 +10,27 @@ GoogleSignIn _googleSignIn = GoogleSignIn(
 );
 
 void main() {
-  runApp(MyApp());
+  runApp(
+      MaterialApp(
+        title: 'Facebook Login',
+        home: MyApp(),
+      ),
+  );
 }
 
-class MyApp extends StatelessWidget {
- @override
-  Widget build(BuildContext context) {
-    var scaffold = Scaffold(
-        backgroundColor: Colors.yellow[50],
-        appBar: AppBar(
-          title: Text("Flutter Login"),
-          backgroundColor: Colors.teal ,
-          ),
-        body: Center(child:_buildBody()),
-      );
-    return MaterialApp(
-      title: "Facebook Login",
-      home: scaffold,
-    );
-  }
+class MyApp extends StatefulWidget {
+  @override
+  State createState() => MyAppState();
+}
 
+class MyAppState extends State<MyApp> {
+  GoogleSignInAccount _currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    _googleSignIn.signInSilently();
+  }
 
   Future<void> _handleSignIn() async {
     try {
@@ -39,16 +40,50 @@ class MyApp extends StatelessWidget {
     }
   }
 
+  Future<void> _handleSignOut() => _googleSignIn.disconnect();
+
   Widget _buildBody() {
-    return Column(
-      mainAxisAlignment:MainAxisAlignment.spaceAround,
+    if (_currentUser != null) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: <Widget>[
-          const Text("You are not currently signed in."),
+          ListTile(
+            leading: GoogleUserCircleAvatar(
+              identity: _currentUser,
+            ),
+            title: Text(_currentUser.displayName ?? ''),
+            subtitle: Text(_currentUser.email ?? ''),
+          ),
+          const Text('Signed in successfully.'),
+          RaisedButton(
+            child: const Text('SIGN OUT'),
+            onPressed: _handleSignOut,
+          ),
+        ],
+      );
+    } else {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          const Text('You are not currently signed in.'),
           RaisedButton(
             child: const Text('SIGN IN'),
             onPressed: _handleSignIn,
           ),
         ],
+      );
+    }
+  }
+
+ @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[50],
+      appBar: AppBar(
+        title: const Text('Flutter Login'),
+        backgroundColor: Colors.teal ,
+      ),
+      body: Center(child:_buildBody()),
     );
-  } 
+  }
 }
